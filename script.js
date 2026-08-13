e839ff97dbfced50ae57553f389e9dff2087bbb2
Parviz <mesbahi@gmx.de>
2026-08-13 15:31:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ function zeigeNachricht() {
     alert("Hallo! Das ist eine Nachricht aus script.js.");
 }
 
-function add() {
+function berechne() {
     const wert1 = Number(document.getElementById("wert1").value);
     const wert2 = Number(document.getElementById("wert2").value);
-    document.getElementById("ergebnis").value = wert1 + wert2;
+    const operation = document.getElementById("operation").value;
+
+    let ergebnis;
+    switch (operation) {
+        case "plus":
+            ergebnis = wert1 + wert2;
+            break;
+        case "minus":
+            ergebnis = wert1 - wert2;
+            break;
+        case "mal":
+            ergebnis = wert1 * wert2;
+            break;
+        case "geteilt":
+            ergebnis = wert1 / wert2;
+            break;
+    }
+
+    document.getElementById("ergebnis").value = ergebnis;
 }
 
 function clearForm() {
