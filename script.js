820bcc98b7997fbb8308d28d8566055f80983fd3
Parviz <mesbahi@gmx.de>
2026-08-13 14:59:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function zeigeNachricht() {
     alert("Hallo! Das ist eine Nachricht aus script.js.");
 }
+
+function add() {
+    const wert1 = Number(document.getElementById("wert1").value);
+    const wert2 = Number(document.getElementById("wert2").value);
+    document.getElementById("ergebnis").value = wert1 + wert2;
+}
