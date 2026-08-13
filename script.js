673c6dda1b28556d2ab753a20224308f2a262701
Parviz <mesbahi@gmx.de>
2026-08-13 15:09:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function add() {
     const wert2 = Number(document.getElementById("wert2").value);
     document.getElementById("ergebnis").value = wert1 + wert2;
 }
+
+function clearForm() {
+    document.getElementById("wert1").value = "";
+    document.getElementById("wert2").value = "";
+    document.getElementById("ergebnis").value = "";
+}
